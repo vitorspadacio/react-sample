f0f5e8ebc6161bec2e337f49363fc3c99c531467
Vitor Spadacio Pisani <vsp193@gmail.com>
2020-12-22 16:39:49 -0300

    Removido axios, adicionado uso da fetchAPI

--- src/features/star-wars/StarWarsApi.ts
+++ src/features/star-wars/StarWarsApi.ts
@@ -1,9 +1,11 @@
-import axios from 'axios'
 import { Planet } from './StarWarsTypes'
 
 export default {
-  getPlanets: (page: number = 1): Promise<Planet> => axios.get(`${(window as any).apis.starwars}planets/?page=${page}`)
-    .then((response) => response.data.results.map((planet) => ({
+  getPlanets: (page: number = 1): Promise<Planet> => fetch(
+    `${(window as any).apis.starwars}planets/?page=${page}`,
+  )
+    .then((response) => response.json())
+    .then((response) => response.results.map((planet) => ({
       name: planet.name,
       diameter: planet.diameter,
       rotationPeriod: planet.rotation_period,
