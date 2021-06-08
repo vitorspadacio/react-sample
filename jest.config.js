dfeb63fef22c9b6f87e1d1a76f87847dd957abc0
Vitor Spadacio Pisani <vsp193@gmail.com>
2021-06-08 00:17:37 -0300

    Ajustes pós-upate

--- jest.config.js
+++ jest.config.js
@@ -1,33 +1,20 @@
 module.exports = {
-  projects: [
-    {
-      displayName: 'test',
-      roots: ['<rootDir>/src'],
-      transform: {
-        '^.+\\.js[x]?$': 'babel-jest',
-        '^.+\\.ts[x]?$': 'ts-jest',
-      },
-      coveragePathIgnorePatterns: [
-        '/index.ts',
-        '/init-redux.ts',
-        '/init-env.ts',
-        '/init-ga.ts',
-        '/test-mock-fetch.ts',
-        '/test-renderer.ts',
-        '/test-run-saga.ts',
-      ],
-      moduleNameMapper: {
-        '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|)$': '<rootDir>/config/file-mock.js',
-        '\\.(css|scss)$': 'identity-obj-proxy',
-      },
-    },
-    {
-      runner: 'jest-runner-eslint',
-      displayName: 'lint',
-      testMatch: [
-        '<rootDir>/src/**/*.test.ts',
-        '<rootDir>/src/**/*.test.tsx',
-      ],
-    },
-  ]
+  displayName: 'test',
+  roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.js[x]?$': 'babel-jest',
+    '^.+\\.ts[x]?$': 'ts-jest',
+  },
+  coveragePathIgnorePatterns: [
+    '/index.ts',
+    '/init-redux.ts',
+    '/init-env.ts',
+    '/test-mock-fetch.ts',
+    '/test-renderer.ts',
+    '/test-run-saga.ts',
+  ],
+  moduleNameMapper: {
+    '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|)$': '<rootDir>/config/file-mock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
 }
