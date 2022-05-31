2d86404f3c9427946957702a2470e9897154cc74
Vitor Spadacio Pisani <vsp193@gmail.com>
2022-05-31 18:36:27 -0300

    test: fix planets page

--- src/infrastructure/test-helpers/test-mock-api.ts
+++ src/infrastructure/test-helpers/test-mock-api.ts
@@ -1,4 +1,1 @@
-// export default (api: object, method: string, mock: any) => jest
-//   .spyOn(api, method as never).mockResolvedValueOnce(mock as never)
-
 export default (method: Function) => (method as jest.Mock)
