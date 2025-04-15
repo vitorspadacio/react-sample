5c488912f7098730cbb716a9472923f7affcc60f
Vitor Spadacio Pisani <vsp193@gmail.com>
2025-04-14 22:13:19 -0300

    chore: atualização de pacotes

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  bracketSameLine: true,
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  printWidth: 120,
+  printWidth: 120,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+};
