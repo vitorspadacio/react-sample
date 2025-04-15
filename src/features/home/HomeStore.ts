f22a56a5be602252a3abc5238d422255f39e94c1
Vitor Spadacio Pisani <vsp193@gmail.com>
2025-04-14 22:13:39 -0300

    feat: removido state para usar zustand na home

---
+++ src/features/home/HomeStore.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand'
+
+export interface HomeStore {
+  number: number
+  increment: () => void
+  decrement: () => void
+  reset: () => void
+}
+
+export const useHomeStore = create<HomeStore>((set => ({
+  number: 0,
+
+  increment: () => set(({number}) => ({ number: number+1 })),
+  decrement: () => set(({number}) => ({ number: number-1 })),
+  reset: () => set(() => ({ number: 0 })),
+})))
+
--- src/features/reducers.ts
+++ src/features/reducers.ts
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux'
 import AuthReducer from './auth/AuthState'
-import HomeReducer from './home/HomeState'
 import NodeSampleReducer from './node-sample/NodeSampleState'
 import StarWarsReducer from './star-wars/StarWarsState'
 import AppReducer from './state'
@@ -9,7 +8,6 @@ import TodoReducer from './todo/TodoState'
 export default combineReducers({
   app: AppReducer,
   auth: AuthReducer,
-  home: HomeReducer,
   nodeSample: NodeSampleReducer,
   starWars: StarWarsReducer,
   todo: TodoReducer,
