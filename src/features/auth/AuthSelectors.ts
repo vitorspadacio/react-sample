10654a3f3dc821cf0a04492a957e9d51020a78f4
Vitor Spadacio Pisani <vsp193@gmail.com>
2025-04-21 11:28:31 -0300

    chore: ajuste

--- src/features/auth/AuthSelectors.ts
+++ src/features/auth/AuthSelectors.ts
@@ -3,5 +3,4 @@ import { AuthStore } from './AuthStore'
 export const selectDisplayName = (state: AuthStore) =>
   state.user?.providerData[0].displayName || 'User'
 
-export const selectHasUser = (state: AuthStore) =>
-  Boolean(state.user?.providerData[0].displayName) ?? false
+export const selectHasUser = (state: AuthStore) => Boolean(state.user?.providerData[0].displayName)
